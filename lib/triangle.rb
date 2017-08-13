c727ecf2400f413810a9ec76c5bfe9d22fb23fad
Opt Fut <optfut2017@gmail.com>
2017-08-13 12:58:27 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
   # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize(first_length, second_length, third_length)
+  	@@first_length = first_length
+  	@@second_length = second_length
+  	@@third_length = third_length
+  end
+
+def valid?
+    (@@first_length > 0 && @@second_length > 0 && @@third_length > 0) &&
+    ((@@first_length + @@second_length > @@third_length) &&
+    (@@first_length + @@third_length > @@second_length) &&
+    (@@second_length + @@third_length > @@first_length))
+  end
+
+  def kind
+  	if !valid? 
+	  		raise TriangleError 
+  	elsif valid? && @@first_length == @@second_length && @@second_length == @@third_length
+	     :equilateral
+  	elsif valid? && @@first_length == @@third_length || @@first_length == @@second_length || @@second_length == @@third_length
+  		 :isosceles
+  	elsif valid? && @@first_length != @@second_length && @@second_length != @@third_length && @@first_length != @@third_length
+  		 :scalene
+  	end
+  end
+end
+
+class TriangleError < StandardError
+	def message
+		"This is not a triangle"
+	end 
 end
